caad75c9d0c044bc965d7d8eedb3552efdb55e77
JulianHernandezMartinez <profefp2223@gmail.com>
2023-10-04 19:45:10 +0200

    Mejoras en Clase

--- 1_Playground/1_Variables y conversiones/script.js
+++ 1_Playground/1_Variables y conversiones/script.js
@@ -5,6 +5,7 @@ const mydiv = document.querySelector('#mydiv');
 let nombre = 'Julian';
 let edad;
 let edadenstring;
+let edadnumero;
 let ciclista = true;
 console.log(nombre);
 console.log(edad)
@@ -16,7 +17,19 @@ console.log(typeof(edad))
 console.log(typeof(ciclista))
 console.log(edad)
 edadenstring=String(edad)
-console.log(edadenstring    )
+console.log(edadenstring)
+edadnumero=Number(edadenstring)
+console.log(edadnumero)
+
+console.log(Boolean(100/0))
+console.log(Boolean('100/0'))
+console.log(Boolean(''))
+console.log(Number('pepe'))
+console.log(Number(Boolean(10/0)))
+
+console.log(15%4)
+console.log(2**4)
+mydiv.textContent = 'Variables y Conversiones?';
 
 
-mydiv.textContent = 'How are you feeling today?';
+
---
+++ 1_Playground/2_Operaciones/script.js
@@ -0,0 +1,29 @@
+'use strict'
+console.log('Hola, PicoCSS!');
+
+const mydiv = document.querySelector('#mydiv');
+
+
+const Nombre = 'Pepe';
+const Apellido = 'Peres'
+
+console.log(15%4)
+console.log(2**4)
+
+console.log (Nombre+' '+Apellido)
+console.log(String(7))
+console.log(+String(7))
+let km = 7;
+
+
+/* Modify-in-place */
+console.log(km+=3,km-=7,km*=2)
+
+console.log(++km,--km, --km,--km)
+console.log(km)
+
+
+mydiv.textContent = 'Variables y Conversiones?';
+
+
+
