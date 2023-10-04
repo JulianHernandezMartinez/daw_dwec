3d51948f79ab7a67c70c260659a63f8e5d14a62d
JulianHernandezMartinez <profefp2223@gmail.com>
2023-10-04 12:49:51 +0200

    Variables JS

---
+++ 1_Playground/1_Variables/script.js
@@ -0,0 +1,22 @@
+'use strict'
+console.log('Hola, PicoCSS!');
+
+const mydiv = document.querySelector('#mydiv');
+let nombre = 'Julian';
+let edad;
+let edadenstring;
+let ciclista = true;
+console.log(nombre);
+console.log(edad)
+edad = null;
+console.log (edad)
+console.log(typeof(edad))
+edad = 25
+console.log(typeof(edad))
+console.log(typeof(ciclista))
+console.log(edad)
+edadenstring=String(edad)
+console.log(edadenstring    )
+
+
+mydiv.textContent = 'How are you feeling today?';
