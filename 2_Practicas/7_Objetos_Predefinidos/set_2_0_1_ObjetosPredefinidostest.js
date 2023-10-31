1ab1e7aaee08c1b373136ccda1e5a335ce79b033
JulianHernandezMartinez <profefp2223@gmail.com>
2023-10-31 14:02:29 +0100

    Ejercicios de Objetos predefinidos JS

---
+++ 2_Practicas/7_Objetos_Predefinidos/set_2_0_1_ObjetosPredefinidostest.js
@@ -0,0 +1,49 @@
+
+'use strict';
+// ejercicio 1
+
+/* dada la fecha actual, en qué día de la 
+semana nos encontramos (lunes, martes...)? */
+
+
+
+
+// Ejercicio2 : 
+/* dada cualquier fecha, devolverla 
+formateada como dd-mm-yyyy */
+
+
+
+// Ejercicio 3: 
+// simular la tirada de una moneda, cara o cruz?
+
+
+
+// Ejercicio 4.
+// dada una cadena de texto y un carácter 
+// (o secuencia de caracteres), 
+// eliminar estos últimos de la cadena de texto
+
+
+
+// Ejercicio 5
+// modificar la función de crear un array (n,min, max) 
+// para que devuelva 
+// un array ordenado (ver método sort())
+
+
+
+// Ejercicio 6: modificar la función de crear un array 
+// y añadirle que en  
+// el caso de proporcionar dos argumentos 
+// (número de elementos y valor mínimo). Si 
+// no se proporciona el tercer parámetro 
+// éste debe ser el valor mínimo más 100.
+
+
+/*Ejercicio 7 : escribir una función que devuelva el 
+número mayor de todos los pasados como argumentos.
+*/
+
+
+
