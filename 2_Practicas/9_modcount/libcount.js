867a59c77eb4fcec5d368eee17a60ac70bafa0f9
JulianHernandezMartinez <profefp2223@gmail.com>
2023-11-08 18:13:53 +0100

    modcount practice for DWEC

---
+++ 2_Practicas/9_modcount/libcount.js
@@ -0,0 +1,24 @@
+
+/**
+ * The counter
+ * @type {number}
+ */
+let counter;
+
+/**
+ * Inits the counter to zero
+ */
+
+
+/**
+ * Increments the counter
+ */
+
+
+/**
+ * Gets the counter
+ * @returns {number}
+ */
+
+
+export { initCounter, incrementCounter, getCounter };
---
+++ 2_Practicas/9_modcount/modcount.js
@@ -0,0 +1,20 @@
+import {  } from "./libcount.js";
+
+// variables
+
+const counterSpn = document.querySelector('#counter');
+const clickmeBtn = document.querySelector('#clickme');
+
+// functions
+
+// listeners & inits
+
+clickmeBtn.addEventListener('click', () => {
+    console.log('clicked!');
+    incrementCounter();
+    const counter = getCounter();
+    console.log(`new counter is ${counter}`);
+    counterSpn.textContent = String(counter);
+});
+
+initCounter();
